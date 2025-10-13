9bac0d9b0296bbddee66dabdb8822a37a1de9c87
sumeetnachane <sumeetnachane@gmail.com>
2025-10-13 15:00:50 +0530

    fix: dynamic import for seedTransactions to fix Prisma build error

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -1,6 +1,7 @@
-import { seedTransactions } from "@/actions/seed";
+// import { seedTransactions } from "@/actions/seed";
 
 export async function GET() {
+  const { seedTransactions } = await import("@/actions/seed"); // dynamic import
   const result = await seedTransactions();
   return Response.json(result); // http://localhost:3000/api/seed  // then u will get success message {"success":true,"message":"Created 171 transactions"}
 }
